8d85debe81f05eb95209551d46e84ebbdfb4b56a
dfab1964 <domenico.fabiano@gmail.com>
2024-12-27 15:17:38 +0100

    Add files via upload

---
+++ assets/js/data-provider.js
@@ -0,0 +1,120 @@
+﻿const jsonBrand = '[{"Id": 189, "Key": "011", "Name": "Loeffler","Description": ""}]';
+
+
+function showUserMessage(msg) {
+    alert(msg);
+}
+
+async function APILogin() {
+    if (window.db_token !== undefined)
+        return;
+
+    try {
+        var body = {
+            dataSource: "server2023q/loeff",
+            userId: "pdm",
+            password: "pdm"
+        };
+
+        const jsonBody = JSON.stringify(body);
+        var url = window.location.origin;
+        // temp
+        //url = 'http://127.0.0.1:5174';
+        //
+
+        const response = await fetch(url + '/api/login', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+                },
+            //body: JSON.stringify({
+            //                dataSource: "server2023q/loeff",
+            //                userId: "pdm",
+            //                password: "pdm",
+            //   }), // Body content as JSON
+            body: jsonBody,
+            }
+        );
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        if (data.token !== null && data.token !== undefined && data.token !== "")
+            window.db_token = data.token;
+
+        return;  
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+        throw error;  // Propagate the error if needed
+    }
+}
+
+function fetchDataFromLocalJson(resource) {
+    var jsonString = '';
+    if (resource == 'brand') {
+        jsonString = jsonBrand;
+    }
+    try {
+        const data = JSON.parse(jsonString);
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log("Invalid JSON:", error);
+    }
+}
+
+async function fetchDataFromAPI(resource) {
+    var jsonString = '';
+    if (resource == "brand") {
+        jsonString = jsonBrand;
+        const data = JSON.parse(jsonString);
+        console.log(data);
+        return data;
+    }
+    try {
+        await APILogin();
+        var url = window.location.origin;
+        // temp
+        //url = 'http://localhost:5174';
+        //
+        const response = await fetch(url + '/api/' + resource, {
+            headers: {
+                'DB-Token': window.db_token,
+                }
+            }
+        );
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;  // Return the data when the fetch is successful
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+        throw error;  // Propagate the error if needed
+    }
+}
+
+function getProducts() {
+    var products = [
+        { name: 'Jennifer', description: "Woman jacket with pockets", 
+            imageURL: "https://raw.githubusercontent.com/dfab1964/image/main/style3.png",
+            colors: [{key:"100", rgb: '#4CAF50', name:'Green'}, {key:"200", rgb: '#FC0F50', name:'Red'}]},
+        { name: 'Marylin', description: "Woman shirt", 
+            imageURL: "https://raw.githubusercontent.com/dfab1964/image/main/style3.jpg",
+            colors: [{key:"100", rgb: '#4CAF50', name:'Green'}, {key:"200", rgb: '#FC0F50', name:'Red'}]},    
+        { name: 'Sandra', description: "Woman shirt with a very long long descripion", 
+            imageURL: "https://raw.githubusercontent.com/dfab1964/image/main/style3.jpg",
+            colors: [{key:"100", rgb: '#4CAF50', name:'Green'}, {key:"200", rgb: '#FC0F50', name:'Red'}]},    
+        { name: 'Magda', description: "Woman shirt", 
+            imageURL: "https://raw.githubusercontent.com/dfab1964/image/main/style3.jpg",
+            colors: [{key:"100", rgb: '#4CAF50', name:'Green'}, {key:"200", rgb: '#FC0F50', name:'Red'}]},   
+        { name: 'Lilly', description: "Woman shirt", 
+            imageURL: "https://raw.githubusercontent.com/dfab1964/image/main/GoLiveMg.345332.jpg",
+            colors: [{key:"100", rgb: '#4CAF50', name:'Green'}, {key:"200", rgb: '#FC0F50', name:'Red'}]},   
+        { name: 'Nicole', description: "Woman shirt", 
+            imageURL: "https://raw.githubusercontent.com/dfab1964/image/main/style3.jpg",
+            colors: [{key:"100", rgb: '#4CAF50', name:'Green'}, {key:"200", rgb: '#FC0F50', name:'Red'}]}    
+    ];
+    
+    return products;
+}
---
+++ assets/js/test.js
@@ -0,0 +1,84 @@
+function generateGUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0,
+        v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+function showMessage(text) {
+    alert(text);
+}
+function handleSelection(el, text){
+    //document.getElementById('btn-brand').innerText=text;
+    el.innerText=text;
+}
+function addRow(container){
+    // empty the container
+   // container.innerHTML = '';
+    // add a new row
+
+    var row = document.getElementById('row-template');
+    
+    var rowClone = row.cloneNode(true);
+    
+    container.appendChild(rowClone);
+    
+}
+
+function addCard(rowContainer, dataItem){
+    //get the column that contains the card
+    var colCard = document.getElementById("col-card-template");
+    
+    // get the element that contains the name
+    var elName = colCard.querySelector("#crd-name");
+    
+    var elDescription = colCard.querySelector('#crd-description');
+    
+    var elImage = colCard.querySelector('#crd-image');
+    
+    // set the value of the name 
+    elName.innerText = dataItem.name;
+    
+    elDescription.innerText = dataItem.description;
+    
+    elImage.src = dataItem.imageURL;
+    
+    // create a clone of the column
+    var colCardClone = colCard.cloneNode(true);
+    
+    addColors(colCard, dataItem.colors)
+    
+    // append the clone to the container    
+    rowContainer.appendChild(colCardClone);
+    
+}
+
+function addColors(container, colors) {
+    for (i=0; i<colors.length; i++) {
+        var textColor = container.querySelector('#div-text-color');
+        textColor.innerText = colors[i].name;    
+    }
+}
+
+
+function addResultChild(){
+    //get the value of the switch
+    var switchValue = 'card';
+    
+    //call the proper function to add either a row or a card
+    if (switchValue=='card'){
+        var rowContainer=document.getElementById('row-card-template');
+        var products = getProducts();
+        for (i=0; i<products.length; i++) {
+            addCard(rowContainer, products[i]);
+        }
+        var colCard = document.getElementById("col-card-template");
+        colCard.style.display = 'none';
+    } 
+    else {
+        var rowContainer=document.getElementById('row-template');
+        addRow(rowContainer);
+    }
+}
+
