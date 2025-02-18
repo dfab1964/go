c586416de7ad37b07abdc7d78a63f9d80cc4f7dc
dfab1964 <domenico.fabiano@gmail.com>
2025-02-18 22:42:44 +0100

    Add files via upload

---
+++ dynamic-ui-helper.js
@@ -0,0 +1,20 @@
+async function fetchClone(htmlFile, elementID) {
+    try {
+            const response = await fetch(htmlFile);  
+            const data = await response.text();
+
+            const tempDiv = document.createElement('div');
+            tempDiv.innerHTML = data;
+
+            const elementToClone = tempDiv.querySelector('#' + elementID); 
+            if (elementToClone) {
+              const clonedElement = elementToClone.cloneNode(true);
+              return clonedElement;
+            } else {
+              console.log("Element to clone not found!");
+            }
+    } catch (error) {
+        console.error('Error fetching or cloning HTML:', error);
+    }
+}
+
